db77ff5dd42593adda6111a35c96db1c0734116a
Munin <munin@SP4620b.serverpronto.com>
2007-04-27 19:04:53 -0400

    Sleipnir finished

--- sleipnir.py
+++ sleipnir.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python 
 
+import traceback
 import psycopg,time,urllib2,sys,os,re
 
 from loadable import planet
 from loadable import galaxy
 from loadable import alliance
+
+sys.path.insert(0, "custom")
 
 import scan
 
@@ -16,18 +19,40 @@ ofile.write("%s" % (os.getpid(),))
 ofile.close()
 
 conn=psycopg.connect("dbname=patools21 user=munin")
+conn.serialize()
+conn.autocommit()
+
+
 cursor=conn.cursor()
+
+
 
 query="SELECT rand_id FROM scanparser_queue"
 
-result=cursor.execute(query)
-
+cursor.execute(query)
+result=cursor.dictfetchall()
 for r in result:
+    rid=str(r['rand_id'])
     query="SELECT rand_id FROM scan WHERE"
-    query+=" tick > (SELECT max_tick()) - 48"
-    query+=" AND rand_id = %s"
-    res=cursor.execute(query,(r['rand_id'],))
+    #    query+=" tick > (SELECT max_tick()) - 72"
+    query+=" rand_id = %s"
+    cursor.execute(query,(rid,))
+    
     if cursor.rowcount > 0:
+        query="DELETE FROM scanparser_queue WHERE rand_id = %s"
+        cursor.execute(query,(rid,))
+        print "Already have scan %s"%(r['rand_id'],)
         continue
 
-    scan.scan(r['rand_id'],None,conn,cursor,'webinterface','webinterface')
+    print "Fetching scan %s"%(rid,)
+    s=scan.scan(rid,None,conn,cursor,'webinterface','webinterface')
+    #s.run()
+    try:
+        s.unsafe_method()
+    except Exception, e:
+        print "Exception in scan: "+e.__str__()
+        traceback.print_exc()
+
+    query="DELETE FROM scanparser_queue WHERE rand_id = %s"
+    cursor.execute(query,(rid,))
+conn.commit()

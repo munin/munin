da4595b3e26d6941061a1a9ca4a59d0c404c6f3d
Andreas Jacobsen <andreaja@ifi.uio.no>
2008-08-03 00:25:40 +0200

    Progress on phone command. Completely untested and probably broken.

--- mod/phone.py
+++ mod/phone.py
@@ -21,20 +21,19 @@ Loadable.Loadable subclass
 
 # This work is Copyright (C)2006 by Andreas Jacobsen
 # Individual portions may be copyright by individual contributors, and
-
 # are included in this collective work with permission of the copyright
 # owners.
 
 
 
-class phane(loadable.loadable):
+class phone(loadable.loadable):
     """
     foo
     """
     def __init__(self,client,conn,cursor):
         loadable.loadable.__init__(self,client,conn,cursor,1000)
         self.commandre=re.compile(r"^"+self.__class__.__name__+"(.*)")
-        self.paramre=re.compile(r"^\s+(\S+)\s+(\S+)")
+        self.paramre=re.compile(r"^\s+(\S+)(\s+(\S+))")
         self.usage=self.__class__.__name__ + " <allow|deny|show> <nick>"
 	self.helptext=None
 
@@ -47,6 +46,11 @@ class phane(loadable.loadable):
             self.client.reply(prefix,nick,target,"You do not have enough access to use this command")
             return 0
 
+        u=loadable.user(pnick=user)
+        if not u.load_from_db(self.conn,self.client,self.cursor):
+            self.client.reply(prefix,nick,target,"You must be registered to use the "+self.__class__.__name__+" command (log in with P and set mode +x)")
+
+
         m=self.paramre.search(m.group(1))
         if not m:
             self.client.reply(prefix,nick,target,"Usage: %s" % (self.usage,))
@@ -54,21 +58,77 @@ class phane(loadable.loadable):
 
         # assign param variables
         command=m.group(1)
-        user=m.group(2)
 
-        u=loadable.user(pnick=user)
-        if not u.load_from_db(self.conn,self.client,self.cursor):
-            self.client.reply(prefix,nick,target,"%s is not a valid user.")
+        if "list".find(command) > 0:
+            self.cursor.execute(query,(username.id,))
+            results=self.cursor.dictfetchall()
+
+            reply=""
+
+            if self.cursor.rowcount < 1:
+                reply="You have no friends. How sad. Maybe you should go post on http://grouphug.us or something."
+            else:
+                people=[]
+
+                for b in self.cursor.dictfetchall():
+                    people.append("%s"%(b['pnick'],))
+                reply="The following people can view your phone number: "
+                reply+=", ".join(people)
+
+            self.client.reply(prefix,nick,target,reply)
             return 1
 
+
+        trustee=m.group(2)
+        t_user=loadable.user(pnick=trustee)
+        if not u.load_from_db(self.conn,self.client,self.cursor):
+            self.client.reply(prefix,nick,target,"%s is not a valid user."%(trustee,))
+            return 0
+
         if "allow".find(command) > 0:
-            pass
+            results=self.phone_query_builder(nick,username,host,target,prefix,command,user,access,"AND t1.friend_id=%s",(t_user.id,))
+            if len(results) > 0:
+                reply="%s can already access your phone number."
+            else:
+                query="INSERT INTO phone (user_id,friend_id) VALUES (%s,%s)"
+                args=(u.id,t_user.id)
+                self.cursor.execute(query,args)
+                reply="Added %s to the list of people able to view your phone number."%(t_user.pnick,)
+            self.client.reply(prefix,nick,target, reply)
+        elif "deny".find(command) > 0:
+            query="DELETE FROM phone WHERE user_id=%s and friend_id=%s"
+            args=(u.id,t_user.id)
+            self.cursor.execute(query,args)
 
+            reply=""
+            if self.cursor.rowcount < 1:
+                reply="Could not find %s among the people allowed to see your phone number."
+            else:
+                reply="Removed %s from the list of people allowed to see your phone number."
+            self.client.reply(prefix,nick,target,reply)
+        elif "show".find(command) > 0:
+            results=self.phone_query_builder(nick,username,host,target,prefix,command,user,access,"AND t1.friend_id=%s",(u.id,))
+            if len(results) < 1:
+                reply="%s won't let you see their phone number. What a paranoid cunt."%(t_user.pnick,)
+            else:
+                reply="%s says his phone number is %s"%(t_user.pnick,t_user.phone)
 
-
-
+            self.client.reply(prefix,nick,target,reply)
 
 
         # do stuff here
 
         return 1
+
+    def phone_query_builder(self,nick,username,host,target,prefix,command,user,access,query_filter=None,query_args=None):
+        args=(user.id,)
+        query="SELECT pnick "
+        query+=" FROM phone AS t1"
+        query+=" INNER JOIN user_list AS t2"
+        query+=" ON t2.id=t1.user_id"
+        query+=" WHERE t1.user_id=%s"
+        if query_filter:
+            query+=query_filter
+            args+=query_args
+
+        self.cursor.execute(query,(query_args,))

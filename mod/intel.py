munin/munin

commit 55c1d4d9600f51ad71c530957ca2f07a6dd6043b

Fix tiny bug in intel.

mod/intel.py
@@ -82,10 +82,11 @@ class intel(loadable.loadable):
 
         opts=self.split_opts(params)
         opts['pid']=p.id
+        
+        a=loadable.alliance(id=None)
         for opt, val in opts.items():
             if opt == "alliance":
                 if val in self.nulls:
-                    a=loadable.alliance(id=None)
                     continue
                 a=loadable.alliance(name=val)
                 if not a.load_most_recent(self.conn,self.client,self.cursor):

commit 6464c1683c2203cfdfb65b2728e862a2ef2ebf72

Fixed awful bug in intel introduced by 55c1d4d9.... Alliance id in intel was being unset.

mod/intel.py
@@ -82,6 +82,9 @@ class intel(loadable.loadable):
 
         opts=self.split_opts(params)
         opts['pid']=p.id
+        a=loadable.alliance(name=i.alliance)
+        if i.alliance:
+            a.load_most_recent(self.conn,self.client,self.cursor)
         for opt, val in opts.items():
             if opt == "alliance":
                 if val in self.nulls:
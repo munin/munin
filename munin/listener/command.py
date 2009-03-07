3139d518fffc410d064bc1dbc49f9e217de39d1e
Andreas Jacobsen <andreaja@ifi.uio.no>
2009-03-07 19:56:49 +0100

    Show error messages on all failures, not just dynamic mods.

--- munin/listener/command.py
+++ munin/listener/command.py
@@ -27,18 +27,19 @@ class command(object):
                                           line   = line)
         if irc_msg.command:
             key = "munin.mod."+irc_msg.command_name
-            if self.control.has_key(key):
-                try:
+            try:
+                if self.control.has_key(key):
                     self.control[key].execute(irc_msg.user,irc_msg.access,irc_msg)
-                except Exception, e:
-                    irc_msg.reply("Error in module '%s'. Please report the command you used to the bot owner as soon as possible."%(irc_msg.command_name,))
-                    raise
-                self.log_command(irc_msg)
-            elif key == 'munin.mod.help':
-                self.help(irc_msg)
-            elif key == 'munin.mod.reincarnate' and irc_msg.access >= 1000:
+                    self.log_command(irc_msg)
+                elif key == 'munin.mod.help':
+                    self.help(irc_msg)
+            except Exception, e:
+                irc_msg.reply("Error in module '%s'. Please report the command you used to the bot owner as soon as possible."%(irc_msg.command_name,))
+                raise
+            if key == 'munin.mod.reincarnate' and irc_msg.access >= 1000:
                 irc_msg.reply("ARISING FROM THE DEAD")
                 raise reboot(irc_msg)
+
 
 
     def log_command(self,irc_msg):

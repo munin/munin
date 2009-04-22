9e968a6341f3b2c27573aa640ed500c6de7adbee
Andreas Jacobsen <andreaja@ifi.uio.no>
2009-04-22 19:25:38 +0200

    Command to show current SMS account balance.

---
+++ munin/mod/showmethemoney.py
@@ -0,0 +1,75 @@
+"""
+Loadable.Loadable subclass
+"""
+
+# This file is part of Munin.
+
+# Munin is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# Munin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Munin; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+
+# This work is Copyright (C)2006 by Andreas Jacobsen
+# Individual portions may be copyright by individual contributors, and
+
+# are included in this collective work with permission of the copyright
+# owners.
+
+import re
+import munin.loadable as loadable
+
+class showmethemoney(loadable.loadable):
+    """
+    foo
+    """
+    def __init__(self,cursor):
+        super(self.__class__,self).__init__(cursor,100)
+        self.commandre=re.compile(r"^"+self.__class__.__name__+"(.*)")
+        self.paramre=re.compile(r"^\s*(STUFFGOESHERE)")
+        self.usage=self.__class__.__name__ + ""
+	self.helptext=None
+
+    def execute(self,user,access,irc_msg):
+        m=self.commandre.search(irc_msg.command)
+        if not m:
+            return 0
+
+        if access < self.level:
+            irc_msg.reply("You do not have enough access to use this command")
+            return 0
+
+        m=self.paramre.search(m.group(1))
+        if not m:
+            irc_msg.reply("Usage: %s" % (self.usage,))
+            return 0
+
+        # assign param variables
+
+        username = self.config.get("clickatell", "user")
+        password = self.config.get("clickatell", "pass")
+        api_id = self.config.get("clickatell", "api")
+
+        ct = Clickatell(username, password, api_id)
+        if not ct.auth():
+            irc_msg.reply("Could not authenticate with server. Super secret message not sent.")
+            return 1
+
+        balance = ct.getbalance()
+
+        if not balance:
+            reply="Help me help you. I need the kwan. SHOW ME THE MONEY"
+        else:
+            reply="Current kwan balance: %d"%(float(balance),)
+
+        irc_msg.reply(reply)
+        return 1

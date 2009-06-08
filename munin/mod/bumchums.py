14267d0d27361d6c33e5a17e073dde53548ef67c
Andreas Jacobsen <andreaja@ifi.uio.no>
2009-06-08 04:27:53 -0400

    Allow alliance names with spaces for apenis and bumchums.

--- munin/mod/bumchums.py
+++ munin/mod/bumchums.py
@@ -40,7 +40,7 @@ class bumchums(loadable.loadable):
     def __init__(self,cursor):
         super(self.__class__,self).__init__(cursor,50)
         self.commandre=re.compile(r"^"+self.__class__.__name__+"(.*)")
-        self.paramre=re.compile(r"^\s+(\S+)\s+(\d+)")
+        self.paramre=re.compile(r"^\s+(.+)\s+(\d+)\s*$")
         self.usage=self.__class__.__name__ + " <alliance> <number>"
         self.helptext=['Pies']
 

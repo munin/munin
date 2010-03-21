bf6a32003f28c748c9567d0f12168caa58e4ad78
Andreas Jacobsen <andreaja@ifi.uio.no>
2010-03-20 20:53:03 -0600

    More graphing stuff.

--- graphing/graphing.rb
+++ graphing/graphing.rb
@@ -5,37 +5,82 @@ require 'gruff'
 require 'graphing/round'
 require 'graphing/pg_accessor.rb'
 
+def draw_graph graph_data
+  g = Gruff::Line.new
+  g.theme = {
+    :colors => ['#ff6600', '#3bb000', '#1e90ff', '#efba00', '#0aaafd'],
+    :marker_color => '#000',
+    :font_color => "grey",
+    :background_colors => ["#0B243B", "#0B243B"]
+  }
+ 
+  g.title = "Round #{graph_data.round_number} #{graph_data.entity_type} #{graph_data.data_type}" 
+  graph_data.data.each do |key, value|
+    g.data(key, value )
+  end
+  
+  # TODO: min_round_tick
+  g.labels = { (500-36) => "500", (1000-36) => "1000"}
+  g.write("Round #{graph_data.round_number}_#{graph_data.entity_type}_#{graph_data.data_type}.png")
+end
 
-rounds = { }
+def create_graph graph_data
+  graph_data.data = PgAccessor.new(db_name).send("get_#{graph_data.entity_type}",graph_data.data_type,rounds[round].send(graph_data.entity_type))
+  draw_graph graph_data
+end
 
-File.open "graphing/graphing.txt" do |infile|
-  counter = 0
-  while (line = infile.gets)
-    line.chomp!
-    if line =~ /^Round \d+$/
-      current = line
-      rounds[current] = Round.new
+class GraphData
+  attr_accessor :round_number, :data, :entity_type, :data_type
+end
+
+
+def process rounds 
+  #rounds.keys.each do |round|
+  ["Round 21"].each do |round|
+    gdata = GraphData.new
+    gdata.round_number = round.split(' ')[1]
+    puts "Fetching data for #{gdata.round_number}"
+    db_name = "patools#{gdata.round_number}" 
+    ["planets","alliances","galaxies"].each do |var|
+      gd = gdata.clone
+      gd.entity_type = var
+      puts "Iterating for #{gd.entity_type} on #{gd.round_number}"
+      [:score, :size, :value].each do |data_type|
+        if(data_type == :value and gd.entity_type == "alliances") then
+          gd.data_type = data_type
+          create_graph gd
+        end
+      end
     end
-    if line =~ /^\D+/
-      rounds[current].alliances = line
-    end
-    if line =~ /^\d+\.\d+\//
-      rounds[current].galaxies = line
-    end
-    if line =~ /\d+\.\d+\.\d+\//
-      rounds[current].planets = line
-    end
-    puts "#{counter}: #{line}"
-    counter = counter + 1
   end
 end
-planets = PgAccessor.new('patools21').get_planets(rounds['Round 21'].planets)
-g = Gruff::Line.new
-g.title = "My Round 21 Planet Graph" 
-planets.each do |key, value|
-  g.data(key, value )
+
+def read_rounds_from_file filename
+  rounds = { }  
+  File.open filename do |infile|
+    counter = 0
+    while (line = infile.gets)
+      line.chomp!
+      if line =~ /^Round \d+$/i
+        current = line
+        rounds[current] = Round.new
+      end
+      if line =~ /^\D+/
+        rounds[current].alliances = line
+      end
+      if line =~ /^\d+\.\d+\//
+        rounds[current].galaxies = line
+      end
+      if line =~ /\d+\.\d+\.\d+\//
+        rounds[current].planets = line
+      end
+      puts "#{counter}: #{line}"
+      counter = counter + 1
+    end
+  end
 end
-g.write('my_fruity_graph.png')
+
+process(read_rounds_from_file("graphing/graphing.txt"))
 
 =begin
 db = PGconn.open :host => "localhost", :port => 5432, :dbname => 'patools21', :user => "munin", :password => "f1r3fly"
@@ -56,3 +101,4 @@ g.labels = {0 => '2003', 2 => '2004', 4 => '2005'}
 g.write('my_fruity_graph.png')
 
 =end
+
--- graphing/pg_accessor.rb
+++ graphing/pg_accessor.rb
@@ -1,25 +1,52 @@
 require 'pg'
-
+require 'pp'
 class PgAccessor
   def initialize db_name
+    @db_name = db_name
     @db = PGconn.open :host => "localhost", :port => 5432, :dbname => db_name, :user => "munin", :password => "f1r3fly"
   end
  
   def get_planets planets
-    get_planet_info(get_planet_identifiers(planets))
+    get_info("planet", get_planet_identifiers(planets))
   end
 
-  def get_planet_info identifiers
+  def get_galaxies galaxies
+    get_info("galaxy", get_galaxy_indentifiers(galaxies))
+  end
+
+  def get_alliances alliances 
+    get_info("alliance", get_alliance_indentifiers(alliances))
+  end
+
+  def get_galaxy_indentifiers galaxies
+    return galaxies.map do |galaxy|
+      puts "#{galaxy} on #{@db_name}"
+      res = @db.exec "select id from galaxy_dump where tick = (select max_tick()) and x=$1 and y=$2", galaxy.split("\.")
+      [galaxy,res[0]['id']]
+    end
+  end
+
+  def get_alliance_indentifiers alliances
+    return alliances.map do |alliance|
+      puts "#{alliance} on #{@db_name}"
+      res = @db.exec "select id from alliance_dump where tick = (select max_tick()) and name ilike $1", [alliance]
+      [alliance,res[0]['id']]
+    end
+  end
+
+  def get_info type, identifiers
     scores = { }
-    identifiers.each do |planet|
-      res = @db.exec "select score from planet_dump where id = $1", [planet[1]]
-      scores[planet[0].gsub(/\./, ':')]=res.to_a.map do |r| r['score'].to_i end
+    identifiers.each do |id|
+      puts "Getting score for #{id} of type #{type}"
+      res = @db.exec "select score from #{type}_dump where id = $1", [id[1]]
+      scores[id[0].gsub(/\./, ':')]=res.to_a.map do |r| r['score'].to_i end
     end
     return scores
   end
 
   def get_planet_identifiers planets
     return planets.map do |planet|
+      puts "#{planet} on #{@db_name}"
       res = @db.exec "select id from planet_dump where tick = (select max_tick()) and x=$1 and y=$2 and z=$3", planet.split("\.")
       [planet,res[0]['id']]
     end

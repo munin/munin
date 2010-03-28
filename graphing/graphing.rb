bebef8db2fd7ebfaabfa731b19449cecb28d24fd
Andreas Jacobsen <andreaja@ifi.uio.no>
2010-03-28 20:16:28 +0200

    More graphing work, mostly getting data for size/score/value and some code cleanup.

--- graphing/graphing.rb
+++ graphing/graphing.rb
@@ -5,10 +5,48 @@ require 'gruff'
 require 'graphing/round'
 require 'graphing/pg_accessor.rb'
 
+
+def digits i
+  count = 0
+  while i > 0
+    count += 1
+    i /= 10
+  end
+  return count
+end
+
+def max_from_all data
+  max = 0
+  data.each do |key, value| 
+    valmax = value.max
+    if valmax > max
+      max = valmax
+    end
+  end
+  return max
+end
+
+def get_increment data
+  max = max_from_all data
+  puts "Max #{max}"
+  order = 10 ** (digits(max) - 1)
+  increment = (order * (max/order)) / 10 
+  return increment
+end
+
+def max_value data
+  max = max_from_all data
+  order = 10 ** (digits(max) - 1)
+  return order * ((max/order)+1)
+end
 def draw_graph graph_data
-  g = Gruff::Line.new
+  g = Gruff::Line.new(1024)
+  y_increment = get_increment graph_data.data
+  puts "y axis increment #{y_increment}"
+  g.y_axis_increment = y_increment
   g.theme = {
-    :colors => ['#ff6600', '#3bb000', '#1e90ff', '#efba00', '#0aaafd'],
+    #:colors => ['#ff6600', '#3bb000', '#1e90ff', '#efba00', '#0aaafd'],
+    :colors => %w(yellow red blue green purple),
     :marker_color => '#000',
     :font_color => "grey",
     :background_colors => ["#0B243B", "#0B243B"]
@@ -20,37 +58,44 @@ def draw_graph graph_data
   end
   
   # TODO: min_round_tick
-  g.labels = { (500-36) => "500", (1000-36) => "1000"}
-  g.write("Round #{graph_data.round_number}_#{graph_data.entity_type}_#{graph_data.data_type}.png")
+  g.labels = { (250-36) => 250, (500-36) => "500", (750-36) => "750", (1000-36) => "1000"}
+  g.minimum_value = 0
+  #g.maximum_value = y_increment * 11 
+  g.write("Round_#{graph_data.round_number}_#{graph_data.entity_type}_#{graph_data.data_type}.png")
 end
 
-def create_graph graph_data
-  graph_data.data = PgAccessor.new(db_name).send("get_#{graph_data.entity_type}",graph_data.data_type,rounds[round].send(graph_data.entity_type))
+def create_graph graph_data, round
+  graph_data.data = PgAccessor.new(graph_data.db_name).send("get_#{graph_data.entity_type}",graph_data.data_type,round.send(graph_data.entity_type))
   draw_graph graph_data
 end
 
 class GraphData
-  attr_accessor :round_number, :data, :entity_type, :data_type
+  attr_accessor :round_number, :data, :entity_type, :data_type, :db_name
+end
+
+def graph_information gd, round, graph_types
+  graph_types.each do |data_type|
+    if not (data_type == :value and gd.entity_type == "alliances") then
+      gd.data_type = data_type
+      create_graph gd, round
+    end
+  end
+
 end
 
 
 def process rounds 
-  #rounds.keys.each do |round|
-  ["Round 21"].each do |round|
+  rounds.keys.each do |round|
+  #["Round 21"].each do |round|
     gdata = GraphData.new
     gdata.round_number = round.split(' ')[1]
     puts "Fetching data for #{gdata.round_number}"
-    db_name = "patools#{gdata.round_number}" 
+    gdata.db_name = "patools#{gdata.round_number}" 
     ["planets","alliances","galaxies"].each do |var|
       gd = gdata.clone
       gd.entity_type = var
       puts "Iterating for #{gd.entity_type} on #{gd.round_number}"
-      [:score, :size, :value].each do |data_type|
-        if(data_type == :value and gd.entity_type == "alliances") then
-          gd.data_type = data_type
-          create_graph gd
-        end
-      end
+      graph_information gd, rounds[round], [:score, :size, :value]
     end
   end
 end
@@ -78,6 +123,7 @@ def read_rounds_from_file filename
       counter = counter + 1
     end
   end
+  return rounds
 end
 
 process(read_rounds_from_file("graphing/graphing.txt"))
--- graphing/pg_accessor.rb
+++ graphing/pg_accessor.rb
@@ -6,16 +6,16 @@ class PgAccessor
     @db = PGconn.open :host => "localhost", :port => 5432, :dbname => db_name, :user => "munin", :password => "f1r3fly"
   end
  
-  def get_planets planets
-    get_info("planet", get_planet_identifiers(planets))
+  def get_planets data_type, planets
+    get_info("planet", data_type, get_planet_identifiers(planets))
   end
 
-  def get_galaxies galaxies
-    get_info("galaxy", get_galaxy_indentifiers(galaxies))
+  def get_galaxies data_type, galaxies
+    get_info("galaxy", data_type, get_galaxy_indentifiers(galaxies))
   end
 
-  def get_alliances alliances 
-    get_info("alliance", get_alliance_indentifiers(alliances))
+  def get_alliances data_type, alliances 
+    get_info("alliance", data_type, get_alliance_indentifiers(alliances))
   end
 
   def get_galaxy_indentifiers galaxies
@@ -34,13 +34,16 @@ class PgAccessor
     end
   end
 
-  def get_info type, identifiers
+  def get_info type, data_type, identifiers
     scores = { }
     identifiers.each do |id|
-      puts "Getting score for #{id} of type #{type}"
-      res = @db.exec "select score from #{type}_dump where id = $1", [id[1]]
-      scores[id[0].gsub(/\./, ':')]=res.to_a.map do |r| r['score'].to_i end
+      puts "Getting #{data_type} for #{id} of type #{type}"
+      query =  "select #{data_type} as result from #{type}_dump where id = $1 order by tick asc"
+      puts query
+      res = @db.exec query, [id[1]]
+      scores[id[0].gsub(/\./, ':')]=res.to_a.map do |r| r['result'].to_i end
     end
+    #puts scores 
     return scores
   end
 

9e5f6beaa789d405bcf51f4d990852340ded3619
Mzyxptlk <grindcore7@hotmail.com>
2013-06-02 03:22:48 -0700

    Dev scans have tooltips since r52

--- munin/custom/scan.py
+++ munin/custom/scan.py
@@ -380,13 +380,13 @@ class scan(threading.Thread):
         args += (scan_id,lightfactory,medfactory,heavyfactory,waveamp,wavedist,metalref,crystalref,eref,reslab,finance,security)
 
         m = re.search("""
-        <tr><td[^>]*>Space\s+Travel</td><td[^>]*>(\d+)</td></tr>\s*
-        <tr><td[^>]*>Infrastructure</td><td[^>]*>(\d+)</td></tr>\s*
-        <tr><td[^>]*>Hulls</td><td[^>]*>(\d+)</td></tr>\s*
-        <tr><td[^>]*>Waves</td><td[^>]*>(\d+)</td></tr>\s*
-        <tr><td[^>]*>Core\s+Extraction</td><td[^>]*>(\d+)</td></tr>\s*
-        <tr><td[^>]*>Covert\s+Ops</td><td[^>]*>(\d+)</td></tr>\s*
-        <tr><td[^>]*>Asteroid\s+Mining</td><td[^>]*>(\d+)</td></tr>
+        <tr><td[^>]*>Space\s+Travel</td><td[^>]*>(\d+)<span class="tooltip">[A-z0-9 ]*</span></td></tr>\s*
+        <tr><td[^>]*>Infrastructure</td><td[^>]*>(\d+)<span class="tooltip">[A-z0-9 ]*</span></td></tr>\s*
+        <tr><td[^>]*>Hulls</td><td[^>]*>(\d+)<span class="tooltip">[A-z0-9 ]*</span></td></tr>\s*
+        <tr><td[^>]*>Waves</td><td[^>]*>(\d+)<span class="tooltip">[A-z0-9 ]*</span></td></tr>\s*
+        <tr><td[^>]*>Core\s+Extraction</td><td[^>]*>(\d+)<span class="tooltip">[A-z0-9 ]*</span></td></tr>\s*
+        <tr><td[^>]*>Covert\s+Ops</td><td[^>]*>(\d+)<span class="tooltip">[A-z0-9 ]*</span></td></tr>\s*
+        <tr><td[^>]*>Asteroid\s+Mining</td><td[^>]*>(\d+)<span class="tooltip">[A-z0-9 ]*</span></td></tr>
         """, page,re.VERBOSE)
 
         travel = m.group(1)

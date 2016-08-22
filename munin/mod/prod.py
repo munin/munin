1058ab41fe661290c003f39e50e3fd2e690e726c
Mzyxptlk <grindcore7@hotmail.com>
2016-08-22 13:55:52 +0200

    Include race production speed bonus in !prod Government prod speed bonus applies additively with population and race bonuses, not multiplicatively Example configuration file expanded to include all government cost bonuses, not just the non-0 ones

--- munin/mod/prod.py
+++ munin/mod/prod.py
@@ -62,13 +62,15 @@ class prod(loadable.loadable):
         # Verify or fix this!
         ship=self.get_ship_from_db(shipname)
 
+        race=ship['race'].lower()[0:3]
+        race_speed = self.config.getfloat('Planetarion', race + '_prod_speed')
+
         if not ship:
             irc_msg.reply("%s is not a ship." % shipname)
             return 0
 
         cost = number * ship['total_cost']
         base_required = 2 * math.sqrt(cost) * self.ln(cost)
-        output = int((4000 * factories) ** 0.98 * (1 + population/100.0))
 
         reply = "Producing %s %s (%s) with %d factories and %d population takes " % (
             self.format_value(number * 100),
@@ -77,39 +79,54 @@ class prod(loadable.loadable):
             factories,
             population)
 
-        # All governments can have their own production speed. Corporatism:
-        corp_speed = 1-float(self.config.get('Planetarion', 'corporatism_prod_speed'))
-        corp_time = int(corp_speed * math.ceil((base_required + (10000 * factories)) / output))
-        reply += "%s ticks with Corporatism" % (corp_time)
+        # All governments can have their own production speed and cost
+        # reduction. Corporatism:
+        corp_bonus = 1-self.config.getfloat('Planetarion', 'corporatism_cost_reduction')
+        corp_speed = self.config.getfloat('Planetarion', 'corporatism_prod_speed')
+        corp_required = 2 * math.sqrt(cost * corp_bonus) * self.ln(cost * corp_bonus)
+        corp_output = int((4000 * factories) ** 0.98 * (1 + population/100.0 + race_speed + corp_speed))
+        corp_time = int(math.ceil(corp_required + 10000 * factories) / corp_output)
+        reply += "%d ticks with Corporatism" % (corp_time)
 
         # Socialism:
-        soci_speed = 1-float(self.config.get('Planetarion', 'socialism_prod_speed'))
-        soci_time = int(soci_speed * math.ceil((base_required + (10000 * factories)) / output))
-        reply += ", %s ticks with Socialism" % (soci_time)
+        soci_bonus = 1-self.config.getfloat('Planetarion', 'socialism_cost_reduction')
+        soci_speed = self.config.getfloat('Planetarion', 'socialism_prod_speed')
+        soci_required = 2 * math.sqrt(cost * soci_bonus) * self.ln(cost * soci_bonus)
+        soci_output = int((4000 * factories) ** 0.98 * (1 + population/100.0 + race_speed + soci_speed))
+        soci_time = int(math.ceil(soci_required + 10000 * factories) / soci_output)
+        reply += ", %d ticks with Socialism" % (soci_time)
 
         # Nationalism:
-        nati_speed = 1-float(self.config.get('Planetarion', 'nationalism_prod_speed'))
-        nati_time = int(nati_speed * math.ceil((base_required + (10000 * factories)) / output))
-        reply += ", %s ticks with Nationalism" % (nati_time)
+        nati_bonus = 1-self.config.getfloat('Planetarion', 'nationalism_cost_reduction')
+        nati_speed = self.config.getfloat('Planetarion', 'nationalism_prod_speed')
+        nati_required = 2 * math.sqrt(cost * nati_bonus) * self.ln(cost * nati_bonus)
+        nati_output = int((4000 * factories) ** 0.98 * (1 + population/100.0 + race_speed + nati_speed))
+        nati_time = int(math.ceil(nati_required + 10000 * factories) / nati_output)
+        reply += ", %d ticks with Nationalism" % (nati_time)
 
         # Democracy:
-        demo_bonus = 1-float(self.config.get('Planetarion', 'democracy_cost_reduction'))
-        demo_speed = 1-float(self.config.get('Planetarion', 'democracy_prod_speed'))
+        demo_bonus = 1-self.config.getfloat('Planetarion', 'democracy_cost_reduction')
+        demo_speed = self.config.getfloat('Planetarion', 'democracy_prod_speed')
         demo_required = 2 * math.sqrt(cost * demo_bonus) * self.ln(cost * demo_bonus)
-        demo_time = int(demo_speed * math.ceil((demo_required + (10000 * factories)) / output))
-        reply += ", %s ticks with Democracy" % (demo_time)
+        demo_output = int((4000 * factories) ** 0.98 * (1 + population/100.0 + race_speed + demo_speed))
+        demo_time = int(math.ceil(demo_required + 10000 * factories) / demo_output)
+        reply += ", %d ticks with Democracy" % (demo_time)
 
         # Totalitarianism:
-        tota_bonus = 1-float(self.config.get('Planetarion', 'totalitarianism_cost_reduction'))
-        tota_speed = 1-float(self.config.get('Planetarion', 'totalitarianism_prod_speed'))
+        tota_bonus = 1-self.config.getfloat('Planetarion', 'totalitarianism_cost_reduction')
+        tota_speed = self.config.getfloat('Planetarion', 'totalitarianism_prod_speed')
         tota_required = 2 * math.sqrt(cost * tota_bonus) * self.ln(cost * tota_bonus)
-        tota_time = int(tota_speed * math.ceil((tota_required + (10000 * factories)) / output))
-        reply += ", %s ticks with Totalitarianism" % (tota_time)
+        tota_output = int((4000 * factories) ** 0.98 * (1 + population/100.0 + race_speed + tota_speed))
+        tota_time = int(math.ceil(tota_required + 10000 * factories) / tota_output)
+        reply += ", %d ticks with Totalitarianism" % (tota_time)
 
         # Anarchy:
-        anar_speed = 1-float(self.config.get('Planetarion', 'anarchy_prod_speed'))
-        anar_time = int(anar_speed * math.ceil((base_required + (10000 * factories)) / output))
-        reply += ", %s ticks with Anarchy." % (anar_time)
+        anar_bonus = 1-self.config.getfloat('Planetarion', 'anarchy_cost_reduction')
+        anar_speed = self.config.getfloat('Planetarion', 'anarchy_prod_speed')
+        anar_required = 2 * math.sqrt(cost * anar_bonus) * self.ln(cost * anar_bonus)
+        anar_output = int((4000 * factories) ** 0.98 * (1 + population/100.0 + race_speed + anar_speed))
+        anar_time = int(math.ceil(anar_required + 10000 * factories) / anar_output)
+        reply += ", %d ticks with Anarchy." % (anar_time)
 
         irc_msg.reply(reply)
 

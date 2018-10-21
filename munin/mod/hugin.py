c6ce1f7b7474c98e1d1529aee478bceeb1461485
Andreas Jacobsen <andreaja@ifi.uio.no>
2018-10-21 20:10:38 +0200

    New command !hugin
    Adds a new command, hugin, which interacts with crontab to manage the
    dumpfile loader. Assumes that run_hugin.sh is the first command in the
    running user's crontab. Allows you to disable (stop), enable (start)
    or view the status of the run_hugin.sh crontab.
    
    Future work may include making the command detect the correct
    placement of the run_hugin.sh command in the user's crontab.

---
+++ munin/mod/hugin.py
@@ -0,0 +1,72 @@
+"""
+Loadable subclass
+"""
+
+# This file is part of Munin.
+
+# Munin is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# Munin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Munin; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+# This work is Copyright (C)2018 by Andreas Jacobsen
+# Individual portions may be copyright by individual contributors, and
+# are included in this collective work with permission of the copyright
+# owners.
+
+import re
+from munin import loadable
+from crontab import CronTab
+
+class hugin(loadable.loadable):
+    def __init__(self, cursor):
+        super(self.__class__, self).__init__(cursor, 1000)
+        self.paramre = re.compile(r"^(?:\s+(start|stop|status))?\s*$")
+        self.usage = self.__class__.__name__ + " [<start|stop|status>]"
+        self.helptext = ['']
+
+    def start(self,ctab,irc_msg):
+        job=ctab[0]
+        if job.is_enabled():
+            irc_msg.reply("Hugin is already spying")
+        else:
+            job.enable()
+            ctab.write()
+            irc_msg.reply("Hugin has been sent spying on the universe")
+
+    def stop(self,ctab,irc_msg):
+        job=ctab[0]
+        if job.is_enabled():
+            job.enable(False)
+            ctab.write()
+            irc_msg.reply("Hugin has been called home to rest")
+        else:
+            irc_msg.reply("Hugin is already home resting")
+
+    def execute(self, user, access, irc_msg):
+        m=self.commandre.search(irc_msg.command)
+        if not m:
+            return 0
+
+        m=self.paramre.search(m.group(1))
+        cmd=m.group(1)
+        ctab=CronTab(user=True)
+
+        if not cmd or cmd == "status":
+            irc_msg.reply("Hugin is %s" % (['out spying','home resting'][ctab[0].is_enabled()],))
+        elif cmd == "start":
+            return self.start(ctab,irc_msg)
+        elif cmd == "stop":
+            return self.stop(ctab,irc_msg)
+        else:
+            irc_msg.reply("Usage: %s" % (self.usage,))
+        return 0

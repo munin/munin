a4010a5d76042da4df3d656dbf691d55eef3e251
Mzyxptlk <grindcore7@hotmail.com>
2022-01-22 11:27:25 +0100

    !maxcap: Support manually specifying attacker coords, consistent with what !xp does

--- munin/mod/maxcap.py
+++ munin/mod/maxcap.py
@@ -33,41 +33,54 @@ from munin import loadable
 class maxcap(loadable.loadable):
     def __init__(self, cursor):
         super().__init__(cursor, 1)
-        self.paramre = re.compile(r"^\s*(\d+)")
-        self.usage = self.__class__.__name__ + " (<total roids>|<x:y:z>)"
+        self.paramre = re.compile(r"^\s*(\d+)(?:[.-:\s](\d+)[.-:\s](\d+)(?:\s+(\d+)[.-:\s](\d+)[.-:\s](\d+))?)?")
+        self.usage = self.__class__.__name__ + " <defender coords> [attacker coords]"
         self.helptext = [
-            'Show how many roids you will cap from a target. If you have your planet set, your actual cap rate will be used, otherwise max cap is assumed.'
+            'Show how many roids you will cap from a target. If you have your planet set, your actual cap rate will be used, otherwise max cap is assumed. You can also manually specify attacker coords.'
         ]
 
     def execute(self, user, access, irc_msg):
-        victim = None
-        m = self.planet_coordre.search(irc_msg.command_parameters)
-        if m:
-            victim = loadable.planet(x=m.group(1), y=m.group(2), z=m.group(3))
-            if not victim.load_most_recent(self.cursor, irc_msg.round):
-                irc_msg.reply(
-                    "%s:%s:%s is not a valid planet" % (victim.x, victim.y, victim.z)
-                )
-                return 1
-            total_roids = victim.size
-        else:
-            m = self.paramre.search(irc_msg.command_parameters)
-            if not m:
-                irc_msg.reply("Usage: %s" % (self.usage,))
-                return 0
-
-            total_roids = int(m.group(1))
-
         if access < self.level:
             irc_msg.reply("You do not have enough access to use this command")
             return 0
 
-        reply = ""
+        victim = None
+        attacker = None
+        m = self.paramre.search(irc_msg.command_parameters)
+        if m:
+            if m.lastindex == 1:
+                total_roids = int(m.group(1))
+            else:
+                victim = loadable.planet(x=m.group(1), y=m.group(2), z=m.group(3))
+                if not victim.load_most_recent(self.cursor, irc_msg.round):
+                    irc_msg.reply(
+                        "%s:%s:%s is not a valid planet" % (victim.x, victim.y, victim.z)
+                    )
+                    return 1
+                total_roids = victim.size
+                if m.lastindex >= 6:
+                    attacker = loadable.planet(x=m.group(4), y=m.group(5), z=m.group(6))
+                    attacker = loadable.planet(x=m.group(4), y=m.group(5), z=m.group(6))
+                    if not attacker.load_most_recent(self.cursor, irc_msg.round):
+                        irc_msg.reply(
+                            "%s:%s:%s is not a valid planet"
+                            % (attacker.x, attacker.y, attacker.z)
+                        )
+                        return 1
+                else:
+                    user = self.load_user_from_pnick(user, irc_msg.round)
+                    if user.planet:
+                        attacker = user.planet
+        else:
+            irc_msg.reply("Usage: %s" % (self.usage,))
+            return 0
+
         cap = 0
         cap_rate = 0.25
-        u = self.load_user_from_pnick(user, irc_msg.round)
-        if u and u.planet and victim:
-            cap_rate = u.planet.cap_rate(victim)
+        if attacker and victim:
+            cap_rate = attacker.cap_rate(victim)
+
+        reply = ""
         for i in range(1, 5):
             cap += int(total_roids * cap_rate)
             reply += "Wave %d: %d (%d), " % (i, int(total_roids * cap_rate), cap)

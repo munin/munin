c65c8ca4007503f9e9b9f9cc4ebe67270acfe9b6
Mzyxptlk <grindcore7@hotmail.com>
2023-02-15 13:17:02 +0100

    !bonus: Include roid bonus repay time

--- munin/mod/bonus.py
+++ munin/mod/bonus.py
@@ -24,16 +24,18 @@ Loadable.Loadable subclass
 # owners.
 
 import re
+import math
 from munin import loadable
 
 
 class bonus(loadable.loadable):
     def __init__(self, cursor):
         super().__init__(cursor, 100)
-        self.paramre = re.compile(r"^(\s*([0-9]+))?$")
-        self.usage = self.__class__.__name__ + " [tick]"
+        self.paramre = re.compile(r"^(?:\s*([0-9.]+))(?:\s*([0-9]+))?\s*$")
+        self.usage = self.__class__.__name__ + " [mining bonus] [tick]"
         self.helptext = [
-            "Show bonus amounts at the specified tick (or the current one if none is given)"
+            "Show bonus amounts at the specified tick (or the current one if "
+            "none is given). You can also pass in your mining bonus."
         ]
 
     def execute(self, user, access, irc_msg):
@@ -42,12 +44,26 @@ class bonus(loadable.loadable):
             irc_msg.reply("You do not have enough access to use this command")
             return 0
 
+        self.cursor.execute("SELECT max_tick(%s::smallint)", (irc_msg.round,))
+        tick = int(self.cursor.fetchone()['max_tick'])
+        bonus = 25
+
         m = self.paramre.search(irc_msg.command_parameters)
-        tick = m.group(2)
-        if tick is None:
-            self.cursor.execute("SELECT max_tick(%s::smallint)", (irc_msg.round,))
-            tick = self.cursor.fetchone()['max_tick']
-        tick = int(tick)
+        if m:
+            try:
+                float_num = float(m.group(1))
+                int_num = int(float_num)
+                if float_num > 200 and m.lastindex < 2 and int_num == float_num:
+                    # Only one argument given and it's an integer too big to be
+                    # a reasonable mining bonus. User probably meant to pass in
+                    # a tick instead.
+                    tick = int_num
+                else:
+                    bonus = float_num
+            except:
+                pass
+            if m.lastindex >= 2:
+                tick = int(m.group(2))
 
         resource_bonus = 10000 + 4800 * tick
         asteroid_bonus = 6 + int(0.15 * tick)
@@ -58,6 +74,7 @@ class bonus(loadable.loadable):
             tick, resource_bonus, asteroid_bonus, research_bonus, construction_bonus,)
 
         u = loadable.user(pnick=irc_msg.user)
+        more = False
         if u.load_from_db(self.cursor, irc_msg.round) and u.planet:
             # Since we don't store the number of roids of each type, assume the
             # roid numbers are about equal.
@@ -69,6 +86,13 @@ class bonus(loadable.loadable):
             )
             if init_cost < resource_bonus:
                 reply += " | You can initiate more roids with the resource bonus than the asteroid bonus gives!"
+                more = True
+        if not more:
+            ticks = math.ceil(resource_bonus / (asteroid_bonus * 250 * (1 + bonus/100.0)))
+            reply += " | You will mine more from the asteroids in %d ticks with %s%% mining bonus" % (
+                ticks,
+                bonus,
+            )
 
         irc_msg.reply(reply)
         return 1
